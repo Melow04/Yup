d3ab8954e736679ad2e0a24a56648d653bbd62b7
Melow04 <corteskal04@gmail.com>
2024-12-15 20:28:51 +0800

    final

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app , server} from "./lib/socket.js";
 
+
 dotenv.config();
 
 
@@ -29,16 +30,14 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.eventNames.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname, "../frontend/MeloChat/dist")));
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/melochat/dist")));
 
 
 
   app.get("*", (req, res) => {
 
-    res.sendFile(path.join(__dirname, "../frontend/MeloChat","dist", "index.html"));
-
-    
+    res.sendFile(path.join(__dirname, "../frontend", "melochat","dist", "index.html"));
   })
 }
 
