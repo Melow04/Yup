a7fac389daea04fb7482f10b7e46dc4731889ccb
Melow04 <154325581+Melow04@users.noreply.github.com>
2024-12-15 20:41:12 +0800

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,14 +29,14 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.eventNames.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname, "../frontend/MeloChat/dist")));
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname, "../frontend/melochat/dist")));
 
 
 
   app.get("*", (req, res) => {
 
-    res.sendFile(path.join(__dirname, "../frontend","MeloChat", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend","melochat", "dist", "index.html"));
 
     
   })
